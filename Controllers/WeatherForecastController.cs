a86abbcddbfeb9b8b3ebd5d0bca2bbd6a011a390
Prakash Dale <Prakash.Dale@Yardi.Com>
2020-06-14 09:39:41 +0530

    build test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace k8stest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","gazab ki"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
